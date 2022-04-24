99260d3cfacf00cc90dad055ba236893ca28447b
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-24 20:14:16 +0900

    1.0(java_pratice 22.04.24 20:14)

--- src/com/javaex/ex08/Tv.java
+++ src/com/javaex/ex08/Tv.java
@@ -8,25 +8,80 @@ public class Tv {
 	private boolean power;
 	
 	// 생성자
+	public Tv(){}
 	
-	
+	public Tv(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+
 	
 	// 메소드 getter-setter
 	
 	public int getChannel() {
 		return channel;
 	}
+
 	public int getVolume() {
 		return volume;
 	}
-	public boolean Power() {
-		return power;
-	}
-	
+
 	
 	// 메소드 일반
 	
+	public void power(boolean on) {
+		if(on = true) {
+			power = true;
+		}else {
+			power = false;
+		}
+
+	}
 	
+	public void channel(int channel) {
+		if(channel>=255) {
+			this.channel = 255;
+		}else if(channel<=1) {
+			this.channel = 1;
+		}
+	}
+	
+	public void channel(boolean up) {
+		if(up = true) {
+			this.channel += 1;
+		}else {
+			this.channel -= 1;
+		}
+	}
+	
+	public void volume(int volume) {
+		if(volume>=255) {
+			volume = 255;
+		}else if(volume<=0) {
+			volume = 0;
+		}
+	}
+	
+	public void volume(boolean up) {
+		if(up == true) {
+			this.volume += 1;
+		}else {
+			this.volume -= 1;
+		}
+	}
+	
+	
+	public void status() {
+		if(power == false) {
+			System.out.println("TV가 꺼져있습니다.");
+		}else {
+			System.out.println("TV가 켜져있습니다.");
+			System.out.println("현재 채널: " + channel);
+			System.out.println("현재 볼륨: " + volume);
+			
+		}
+	}
 	
 	
 	
